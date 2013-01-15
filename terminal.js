e0f104daaf46ad2198a099103791023b280aa735
mbilker <me@mbilker.us>
2013-01-14 20:25:55 -0500

    Made some changes to the terminal client

--- terminal.js
+++ terminal.js
@@ -7,23 +7,23 @@ var sock = net.connect(1337, '192.168.1.14')
 process.stdin.pipe(sock)
 sock.pipe(process.stdout)
  
-sock.on('connect', function () {
+sock.on('connect', function() {
   process.stdin.resume();
   process.stdin.setRawMode(true)
 })
  
-sock.on('close', function done () {
+sock.on('close', function() {
   process.stdin.setRawMode(false)
   process.stdin.pause()
   sock.removeListener('close', done)
 })
  
-process.stdin.on('end', function () {
+process.stdin.on('end', function() {
   sock.destroy()
   console.log()
 })
  
-process.stdin.on('data', function (b) {
+process.stdin.on('data', function(b) {
   if (b.length === 1 && b[0] === 4) {
     process.stdin.emit('end')
   }
